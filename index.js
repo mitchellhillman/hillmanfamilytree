3b225a14ab738398d990145359ca38f8c52bd3e9
Mitchell Hillman <mitchell.hillman@bottomline.com>
2021-10-12 16:34:02 -0400

    generation rows

--- index.js
+++ index.js
@@ -24,62 +24,84 @@ const buildPerson = (startId) => {
   };
 };
 
-const getName = (person) =>
-  `${person.firstname} ${person.middlename ? person.middlename + " " : ""}${
-    person.lastname
-  } ${person.suffix}`;
-
 const createTable = (startPerson) => {
   const table = [];
 
   const createRow = (person, count = 0) => {
     if (!person) return;
-    if(!person) return
+    if (!person) return;
     if (person.parents?.length) {
-      count++      
+      count++;
       const couple = person.parents.map((parent) => createRow(parent, count));
-      if(table[count]) {
-        console.log("table[count]", table[count]);
-        table[count].push(couple)
+      if (table[count]) {
+        table[count].push(couple);
       } else {
-        table[count] = [couple]
+        table[count] = [couple];
       }
     }
-    return getName(person);
+    return person;
   };
 
-  table.unshift(createRow(startPerson));   
+  table.unshift(createRow(startPerson));
 
-  return table;
+  return table.filter((n) => n);
 };
 
 const myTree = createTable(buildPerson("3"));
-const renderSVG = (tree) => {
-  const table = `
 
-<svg width="1000" height="1000" xmlns="http://www.w3.org/2000/svg">
-     <style>
-      text {
-        font-family: sans-serif;
-        font-size: 16px;
-      }
-      .node {
-        stroke-linecap: round;
-      }
-      .link {
-        fill: none;
-      }
-    </style>
-      ${tree.map(
-        (branch, index) => branch.length ? : `<text y="${index * 20}">${branch}</text>`
-      )}     
-    </svg>
-  
-  `;
-  return table;
+const nodeWidth = 200;
+
+const Name = ({ person }) => {
+  return person
+    ? `${person.firstname} ${person.middlename ? person.middlename + " " : ""}
+      ${person.lastname} ${person.suffix}`
+    : "";
 };
 
-console.log("myTree", myTree);
+const Person = ({ person, genIndex, coupleIndex, personIndex }) => {
+  return `<text x="${
+    personIndex * nodeWidth + coupleIndex * (nodeWidth * 3)
+  }" y="${(genIndex + 1) * 40}">${Name({ person })}</text>`;
+};
+
+const Tree = (tree) => {
+  return tree.map((gen, genIndex) => {
+    if (Array.isArray(gen)) {
+      return gen.map((couple, coupleIndex) => {
+        return couple.map((person, personIndex) =>
+          Person({ person, coupleIndex, genIndex, personIndex })
+        );
+      });
+    } else {
+      return Person({
+        person: gen,
+        genIndex: 0,
+        coupleIndex: 0,
+        personIndex: 0,
+      }); // first gen
+    }
+  });
+};
+
+const renderSVG = (tree) => {
+  return `
+    <svg width="1000" height="1000" xmlns="http://www.w3.org/2000/svg">
+     <style>
+        text {
+          font-family: sans-serif;
+          font-size: 16px;
+        }
+        .node {
+          stroke-linecap: round;
+        }
+        .link {
+          fill: none;
+        }
+      </style>
+      ${Tree(tree)}     
+    </svg>
+  `;
+};
 
 fs.writeFile("familytree.svg", renderSVG(myTree), () => {
   console.log("SVG created");
