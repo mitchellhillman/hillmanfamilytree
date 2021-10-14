f270b22245fb8e0e528acbf85566d8f2f0089c41
Mitchell Hillman <mitchell.hillman@bottomline.com>
2021-10-14 11:49:38 -0400

    add partners to patriarch tree

--- script.js
+++ script.js
@@ -10,6 +10,33 @@ const getDates = ({ birthdate, deathdate }) => {
   return (birthdate || deathdate) && `${birthYear} - ${deathYear}`;
 };
 
+const enrichPersons = (family) => family.map(person => {
+  const children = family.reduce((acc, curr) => {
+    if (curr.father === person.id || curr.mother === person.id) {
+      acc.push(curr);
+    }
+    return acc;
+  }, []);
+
+  const fatherIds = children.map(child => child.father);
+  const motherIds = children.map(child => child.mother);
+  const parentIds = [...fatherIds, ...motherIds]
+    .reduce((acc, curr) => {
+      if (!acc.includes(curr)) acc.push(curr);
+      return acc;
+    }, [])
+    .filter(id => id !== person.id);
+
+  const partners = family.reduce((acc, curr) => {
+    if (parentIds.includes(curr.id)) {
+      acc.push(curr);
+    }
+    return acc;
+  }, []);
+
+  return { ...person, children, partners };
+});
+
 const buildHeritageTree = (family, personId) => {
   const person = family.filter(({ id }) => id === personId)[0];
   const father = family.reduce((acc, curr) => {
@@ -35,18 +62,7 @@ const buildHeritageTree = (family, personId) => {
 };
 
 const buildPatriarchTree = (family, personId) => {
-  const withChildren = family.map(person => {
-    const children = family.reduce((acc, curr) => {
-      if (curr.father === person.id || curr.mother === person.id) {
-        acc.push(curr);
-      }
-      return acc;
-    }, []);
-
-    return { ...person, children };
-  });
-
-  const person = withChildren.filter(({ id }) => id === personId)[0];
+  const person = family.filter(({ id }) => id === personId)[0];
 
   const children = family.reduce((acc, curr) => {
     if (curr.father === person.id || curr.mother === person.id) {
@@ -60,7 +76,123 @@ const buildPatriarchTree = (family, personId) => {
   };
 };
 
-const chart = (data) => {
+const patriarchTree = (data) => {
+  const width = 2000;
+
+  const tree = d => {
+    const root = d3.hierarchy(d);
+    root.dx = 100;
+    root.dy = width / (root.height + 1);
+    return d3.tree().nodeSize([root.dx, root.dy])(root);
+  };
+
+  const root = tree(data);
+
+  let x0 = Infinity;
+  let x1 = -x0;
+  root.each(d => {
+    if (d.x > x1) x1 = d.x;
+    if (d.x < x0) x0 = d.x;
+  });
+
+  const marginRightLeft = 80;
+
+  const svg = d3.create('svg')
+    .attr('viewBox', [0, 0, width + (marginRightLeft * 2), x1 - x0 + root.dx * 2]);
+
+  const g = svg.append('g')
+    .attr('font-family', 'sans-serif')
+    .attr('font-size', 10)
+    .attr('transform', `translate(${marginRightLeft + (root.dy / 3)}, ${root.dx - x0 })`);
+
+  g.append('g')
+    .attr('fill', 'none')
+    .attr('stroke', '#ddd')
+    .attr('stroke-opacity', 1)
+    .attr('stroke-width', 1)
+    .selectAll('path')
+    .data(root.links())
+    .join('path')
+    .attr('d', d3.linkHorizontal()
+      .x(d => d.y)
+      .y(d => d.x));
+
+  const node = g.append('g')
+    .attr('stroke-linejoin', 'round')
+    .attr('stroke-width', 3)
+    .selectAll('g')
+    .data(root.descendants())
+    .join('g')
+    .attr('transform', d => `translate(${d.y},${d.x})`);
+
+  node.append('circle')
+    .attr('fill', d => d.data.gender === 'f' ? '#d742f5' : '#0c74a8')
+    .attr('r', 2.5);
+
+  node.append('text')
+    .attr('dy', '0.31em')
+    .attr('x', -2)
+    .attr('y', 10)
+    .attr('text-anchor', 'start')
+    .text(d => getName(d.data))
+    .clone(true)
+    .lower()
+    .attr('stroke', 'white');
+
+  node.append('text')
+    .attr('dy', '0.31em')
+    .attr('x', -2)
+    .attr('y', 20)
+    .attr('text-anchor', 'start')
+    .text(d => getDates(d.data))
+    .clone(true)
+    .lower()
+    .attr('stroke', 'white');
+
+  const partners = root.descendants().reduce((acc, curr) => {
+    if (curr.data.partners.length) {
+      acc.push({ ...curr, data: curr.data.partners[0] });
+    }
+    return acc;
+  }, []);
+
+  // partner
+  const partner = g.append('g')
+    .attr('stroke-linejoin', 'round')
+    .attr('stroke-width', 3)
+    .selectAll('g')
+    .data(partners)
+    .join('g')
+    .attr('transform', d => `translate(${d.y},${d.x + 50})`);
+
+  partner.append('circle')
+    .attr('fill', d => d.data.gender === 'f' ? '#d742f5' : '#0c74a8')
+    .attr('r', 2.5);
+
+  partner.append('text')
+    .attr('dy', '0.31em')
+    .attr('x', -2)
+    .attr('y', 10)
+    .attr('text-anchor', 'start')
+    .text(d => getName(d.data))
+    .clone(true)
+    .lower()
+    .attr('stroke', 'white');
+
+  partner.append('text')
+    .attr('dy', '0.31em')
+    .attr('x', -2)
+    .attr('y', 20)
+    .attr('text-anchor', 'start')
+    .text(d => getDates(d.data))
+    .clone(true)
+    .lower()
+    .attr('stroke', 'white');
+
+  return svg.node();
+};
+
+const heritageTree = (data) => {
   const width = 1500;
 
   const tree = d => {
@@ -110,7 +242,6 @@ const chart = (data) => {
     .attr('transform', d => `translate(${d.y},${d.x})`);
 
   node.append('circle')
-    .attr('fill', '#ddd')
     .attr('fill', d => d.data.gender === 'f' ? '#d742f5' : '#0c74a8')
     .attr('r', 2.5);
 
@@ -141,10 +272,11 @@ const run = async () => {
   document.querySelector('#patriarch').innerHTML = 'Loading...';
   document.querySelector('#heritage').innerHTML = 'Loading...';
   const hillmanFamily = await d3.csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQbWzAXR72DPLKrTIOAN4hyKeasXYV7Qukdu_wbgG5_tnSVUaQvorQ3lH8Xrs0j0uwR0WUhuGAuPrtY/pub?output=csv');
+  const enrichedFamily = enrichPersons(hillmanFamily);
   document.querySelector('#patriarch').innerHTML = '';
   document.querySelector('#heritage').innerHTML = '';
-  document.querySelector('#patriarch').appendChild(chart(buildPatriarchTree(hillmanFamily, '39')));
-  document.querySelector('#heritage').appendChild(chart(buildHeritageTree(hillmanFamily, '200')));
+  document.querySelector('#patriarch').appendChild(patriarchTree(buildPatriarchTree(enrichedFamily, '39')));
+  document.querySelector('#heritage').appendChild(heritageTree(buildHeritageTree(hillmanFamily, '200')));
 };
 
 run();
